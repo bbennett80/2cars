37ea3cd2ffed82a40170ad230869ffb5dbb0b407
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-09-11 16:39:21 -0700

    Create car.sh

---
+++ example/car.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CAR1="CAR1"
+CAR2="CAR2"
+
+cd data/
+
+if [ ! -d "$CAR1" ] && [ ! -f "$CAR1" ]; then
+    echo "Switching to $CAR2."
+    mv openpilot $CAR1 && mv $CAR2 openpilot
+    echo "Now operating on $CAR2."
+elif [ ! -d "$CAR2" ] && [ ! -f "$CAR2" ]; then
+    echo "Switching to $CAR1."
+    mv openpilot $CAR2 && mv $CAR1 openpilot
+    echo "Now operating on $CAR1."
+fi
